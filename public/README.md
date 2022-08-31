37279d7279d008023d2d24abc1dc681ac90c4af7
Jdubbs702 <702leopold@gmail.com>
2022-08-31 07:50:55 +0300

    firebase deployment added

--- public/README.md
+++ public/README.md
@@ -1,10 +1,12 @@
+https://nasdaqindex.web.app
+
 Milestone 4
 
 Features
 Create a marquee at the top of the main page showing current stock information
 Search in the API docs (link in the summary), for the endpoint you need to use to get a list of companies and their current stock price
 Animate the the marquee, to look like in a stock market (example below)
-We recommend you will use keyframes and animation property in CSS, you can learn about it here animation	
+We recommend you will use keyframes and animation property in CSS, you can learn about it here animation
 Also, add favicon to the website (you can look for icons online)
 You can present only a subset of the items if the browser stuck when trying to present too many items
 Do not use the <marquee> tag. (see the alert here: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/marquee)
@@ -17,4 +19,3 @@ This class should accept an element in the constructor argument, and render all 
 This class should be implement in a separate file called Marquee.js (which should be imported in the main page html file)
 In your main html file, you should only have an empty html element, which you pass to your Marquee instance constructor, and call this instance method to load and render everything to this div
 If you want, you can use a function contstructor instead of a class (does the same)
-
---
+++ public/company.js
@@ -0,0 +1,46 @@
+let symbol2;
+const spinner = grabElement("thisChartRightHere");
+const pageSpinner = grabElement("compContainer");
+window.onload = async function () {
+  //get symbol and fetch URL with symbol
+  try {
+    const url_string = window.location.href;
+    console.log(url_string);
+    const url = new URL(url_string);
+    let symbol = url.searchParams.get("symbol");
+    symbol2 = symbol; //send symbol to global scope
+    console.log(symbol);
+    pageSpinner.classList.add("spinner-border");
+    profileData = await getProfile(symbol);
+    await createProfile(profileData);
+    pageSpinner.classList.remove("spinner-border");
+    chartIt();
+  } catch (err) {
+    console.log("Issues with Parsing URL Parameter's - " + err);
+  }
+};
+
+const xlabels = [];
+const ylabels = [];
+async function chartIt() {
+  spinner.classList.add("spinner-border");
+  await fetchHistory(symbol2);
+  spinner.classList.remove("spinner-border");
+  const ctx = document.getElementById("thisChartRightHere").getContext("2d");
+  const myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: xlabels,
+      datasets: [
+        {
+          label: "Stock Price History",
+          data: ylabels,
+          fill: true,
+          backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+          borderColor: ["rgba(255, 99, 132, 1)"],
+          borderWidth: 1,
+        },
+      ],
+    },
+  });
+}
